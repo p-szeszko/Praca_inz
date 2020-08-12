301358c52b8ace0ec09682c3c2f3807c7325f736
p-szeszko <przemekszeszko1@gmail.com>
2020-08-12 13:15:17 +0200

    Dialog window

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {  MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from "@angular/material/dialog";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SiteDialog} from './app.component';
 import { HeaderComponent } from './my-site/header/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,14 +15,17 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SiteDialog
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatDialogModule,
     MatButtonModule,
     HttpClientModule
   ],
